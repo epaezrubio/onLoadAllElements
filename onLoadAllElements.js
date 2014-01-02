ca5f85e29246e087b65fae08f8baed341f6e012f
Eduardo Páez Rubio <epaezrubio@gmail.com>
2014-01-03 00:16:46 +0100

    Double semicolon fixed

--- onLoadAllElements.js
+++ onLoadAllElements.js
@@ -2,7 +2,7 @@ window[addEventListener ? 'addEventListener' : 'attachEvent']( addEventListener 
 	var e = document.querySelectorAll("[onload]:not(body):not(frame):not(frameset):not(iframe):not(img):not([type='image']):not(link):not(script):not(style)");
 	for (var i = 0; i < e.length; i++) {
 		console.log(e[i]);
-		var script = e[i].getAttribute("onload").replace(new RegExp("(?!A-z0-9)this(?![A-z0-9])", "g"), "e[" + i + "]");;
+		var script = e[i].getAttribute("onload").replace(new RegExp("(?!A-z0-9)this(?![A-z0-9])", "g"), "e[" + i + "]");
 		eval(script);
 	}
 })
